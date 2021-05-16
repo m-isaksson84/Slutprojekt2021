714d0e5a695144a879d1f7c04763756a84e27821
m-isaksson84 <marcus.isaksson@elev.ga.ntig.se>
2021-05-16 20:11:33 +0200

    slutförde projektet, fixade damage metod osv.

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -9,9 +9,13 @@ namespace Slutprojekt
             
             Random generator = new Random();
             bool inMenu = true;
+            bool inGameStartup = false;
             bool gameStarted = false;
             string classChosen = "";
+            string botClassChosen = "";
             int botClass = generator.Next(1, 4);
+            int playerHealth = 0;
+            int botHealth = 0;
 
             while (inMenu) {
 
@@ -23,37 +27,6 @@ namespace Slutprojekt
                 Console.WriteLine("");
                 
                 string classSelect = Console.ReadLine();
-
-
-               static string GetDamage(string classChosen)
-                {   
-                    // input = det som användaren skriver.
-                    //string input = Console.ReadLine();
-
-                    string test = "melee AAAasbyuefg8yeuf";
-                    string test2 = "archer AAAasbyuefg8yeuf";
-                    string test3 = "mage AAAasbyuefg8yeuf";
-                    string test4 = "How the hell??!";
-
-                    // jämför inputLength med det bestämda intervallet.
-                    if (classChosen == "1") {
-                        return test;
-                    }
-
-                    if (classChosen == "2") {
-                        return test2;
-                    }
-
-                    if (classChosen == "3") {
-                        return test3;
-                    }
-
-                    else {
-                        return test4;
-                    }
-                }
-
-                
 
                 if (classSelect == "1") {
 
@@ -68,7 +41,7 @@ namespace Slutprojekt
                         Console.WriteLine("");
                         Console.WriteLine("==========================");
                         Console.WriteLine("");
-                        Console.WriteLine("     Health: ♥♥♥ (30)");
+                        Console.WriteLine("     Health: ♥♥♥ (150)");
                         Console.WriteLine("      Damage: 5-10");
                         Console.WriteLine("");
                         Console.WriteLine("==========================");
@@ -94,7 +67,7 @@ namespace Slutprojekt
                             Console.Beep(294,10);
                             // stänger ned / öppnar de "flikar" som ska / inte ska synas
                             inMeleeInfo = false;
-                            gameStarted = true;
+                            inGameStartup = true;
                             // här deklareras den "klass" som spelaren valt att spela som
                             classChosen = "1";
                         }
@@ -114,7 +87,7 @@ namespace Slutprojekt
                         Console.WriteLine("");
                         Console.WriteLine("==========================");
                         Console.WriteLine("");
-                        Console.WriteLine("     Health: ♥♥ (20)");
+                        Console.WriteLine("     Health: ♥♥ (100)");
                         Console.WriteLine("      Damage: 1-20");
                         Console.WriteLine("");
                         Console.WriteLine("==========================");
@@ -140,7 +113,7 @@ namespace Slutprojekt
                             Console.Beep(294,10);
                             // stänger ned / öppnar de "flikar" som ska / inte ska synas
                             inArcherInfo = false;
-                            gameStarted = true;
+                            inGameStartup = true;
                             // här deklareras den "klass" som spelaren valt att spela som
                             classChosen = "2";
                         }
@@ -160,7 +133,7 @@ namespace Slutprojekt
                         Console.WriteLine("");
                         Console.WriteLine("==========================");
                         Console.WriteLine("");
-                        Console.WriteLine("     Health: ♥♥ (20)");
+                        Console.WriteLine("     Health: ♥♥ (100)");
                         Console.WriteLine("      Damage: 5-15");
                         Console.WriteLine("");
                         Console.WriteLine("==========================");
@@ -186,21 +159,23 @@ namespace Slutprojekt
                             Console.Beep(294,10);
                             // stänger ned / öppnar de "flikar" som ska / inte ska synas
                             inMageInfo = false;
-                            gameStarted = true;
+                            inGameStartup = true;
                             // här deklareras den "klass" som spelaren valt att spela som
                             classChosen = "3";
                         }
                     }
                 }
                 
-                while (gameStarted) {
+                while (inGameStartup) {
 
-                    Random randomUpdating = new Random();
+                    //Random randomUpdating = new Random();
                     
                     string botClassInfo = "";
 
                     if (botClass.ToString() == "1") {
+                        botClassChosen = "1";
                         botClassInfo = "Melee";
+                        botHealth = 150;
                         Console.WriteLine("Your opponent chose " + botClassInfo);
                         Console.WriteLine("");
                         Console.WriteLine("==========================");
@@ -209,7 +184,7 @@ namespace Slutprojekt
                         Console.WriteLine("");
                         Console.WriteLine("==========================");
                         Console.WriteLine("");
-                        Console.WriteLine("     Health: ♥♥♥ (30)");
+                        Console.WriteLine("     Health: ♥♥♥ (150)");
                         Console.WriteLine("      Damage: 5-10");
                         Console.WriteLine("");
                         Console.WriteLine("==========================");
@@ -217,7 +192,9 @@ namespace Slutprojekt
                     }
 
                     if (botClass.ToString() == "2") {
+                        botClassChosen = "2";
                         botClassInfo = "Archer";
+                        botHealth = 100;
                         Console.WriteLine("Your opponent chose " + botClassInfo);
                         Console.WriteLine("==========================");
                         Console.WriteLine("");
@@ -225,7 +202,7 @@ namespace Slutprojekt
                         Console.WriteLine("");
                         Console.WriteLine("==========================");
                         Console.WriteLine("");
-                        Console.WriteLine("     Health: ♥♥ (20)");
+                        Console.WriteLine("     Health: ♥♥ (100)");
                         Console.WriteLine("      Damage: 1-20");
                         Console.WriteLine("");
                         Console.WriteLine("==========================");
@@ -233,7 +210,9 @@ namespace Slutprojekt
                     }
 
                     if (botClass.ToString() == "3") {
+                        botClassChosen = "3";
                         botClassInfo = "Magic";
+                        botHealth = 100;
                         Console.WriteLine("Your opponent chose " + botClassInfo);
                         Console.WriteLine("==========================");
                         Console.WriteLine("");
@@ -241,74 +220,186 @@ namespace Slutprojekt
                         Console.WriteLine("");
                         Console.WriteLine("==========================");
                         Console.WriteLine("");
-                        Console.WriteLine("     Health: ♥♥♥ (30)");
-                        Console.WriteLine("      Damage: 5-10");
+                        Console.WriteLine("     Health: ♥♥ (100)");
+                        Console.WriteLine("      Damage: 5-15");
                         Console.WriteLine("");
                         Console.WriteLine("==========================");
                     }
 
                     if (classChosen == "1") {
-                        string damage = GetDamage(classChosen);
-
-                        Console.WriteLine(damage);
-                        Console.WriteLine(classChosen);
+                        playerHealth = 150;
                         Console.WriteLine("==========================");
                         Console.WriteLine("");
                         Console.WriteLine("      Your stats:");
                         Console.WriteLine("");
                         Console.WriteLine("==========================");
                         Console.WriteLine("");
-                        Console.WriteLine("     Health: ♥♥♥ (30)");
+                        Console.WriteLine("     Health: ♥♥♥ (150)");
                         Console.WriteLine("      Damage: 5-10");
                         Console.WriteLine("");
                         Console.WriteLine("==========================");
                         Console.WriteLine("");
-                        int meleeDamage = generator.Next(5, 11);
-                        Console.ReadLine();
-                        Console.Clear();
+                        Console.WriteLine("  Press '1' to start game.");
+                        Console.WriteLine("");
+                        Console.WriteLine("==========================");
+                        Console.WriteLine("");
+                        //int meleeDamage = generator.Next(5, 11);
                     }
 
                     if (classChosen == "2") {
-                        string damage = GetDamage(classChosen);
-
-                        Console.WriteLine(damage);
-                        Console.WriteLine(classChosen);
+                        playerHealth = 100;
                         Console.WriteLine("==========================");
                         Console.WriteLine("");
                         Console.WriteLine("      Your stats:");
                         Console.WriteLine("");
                         Console.WriteLine("==========================");
                         Console.WriteLine("");
-                        Console.WriteLine("     Health: ♥♥ (20)");
+                        Console.WriteLine("     Health: ♥♥ (100)");
                         Console.WriteLine("      Damage: 1-20");
                         Console.WriteLine("");
                         Console.WriteLine("==========================");
-                        int rangedDamage = generator.Next(1, 21);
-                        Console.ReadLine();
-                        Console.Clear();
+                        Console.WriteLine("");
+                        Console.WriteLine("  Press '1' to start game.");
+                        Console.WriteLine("");
+                        Console.WriteLine("==========================");
+                        Console.WriteLine("");
+                        //int rangedDamage = generator.Next(1, 21);
                     }
 
                     if (classChosen == "3") {
-                        string damage = GetDamage(classChosen);
-
-                        Console.WriteLine(damage);
-                        Console.WriteLine(classChosen);
+                        playerHealth = 100;
                         Console.WriteLine("==========================");
                         Console.WriteLine("");
                         Console.WriteLine("      Your stats:");
                         Console.WriteLine("");
                         Console.WriteLine("==========================");
                         Console.WriteLine("");
-                        Console.WriteLine("     Health: ♥♥ (20)");
+                        Console.WriteLine("     Health: ♥♥ (100)");
                         Console.WriteLine("      Damage: 5-15");
                         Console.WriteLine("");
                         Console.WriteLine("==========================");
-                        int magicDamage = generator.Next(5, 16);
+                        Console.WriteLine("");
+                        Console.WriteLine("  Press '1' to start game.");
+                        Console.WriteLine("");
+                        Console.WriteLine("==========================");
+                        Console.WriteLine("");
+                        //int magicDamage = generator.Next(5, 16);
+                    }
+
+                        string select = Console.ReadLine();
+
+                        if (select == "1") {
+                            Console.Clear();
+                            Console.Beep(294,10);
+                            // stänger ned / öppnar de "flikar" som ska / inte ska synas
+                            inGameStartup = false;
+                            gameStarted = true;
+                            // här deklareras den "klass" som spelaren valt att spela som
+                            classChosen = "3";
+                        }
+
+                        else {
+                            Console.Clear();
+                            inGameStartup = true;
+                        }
+                }
+
+                while (gameStarted) {
+
+                    static int GetPlayerDamage(string classChosen)
+                    {   
+                
+                        Random damageGenerator = new Random();
+                        int botClass1 = damageGenerator.Next(5, 11);
+                        int botClass2 = damageGenerator.Next(1, 21);
+                        int botClass3 = damageGenerator.Next(5, 11);
+                        int botClass4 = damageGenerator.Next(0, 999999);
+
+                        if (classChosen == "1") {
+                            return botClass1;
+                        }
+
+                        if (classChosen == "2") {
+                            return botClass2;
+                        }
+
+                        if (classChosen == "3") {
+                            return botClass3;
+                        }
+
+                        else {
+                            return botClass4;
+                        }
+                    }
+
+                    static int GetBotDamage(string botClassChosen)
+                    {   
+
+                        Random damageGenerator = new Random();
+                        int damageClass1 = damageGenerator.Next(5, 11);
+                        int damageClass2 = damageGenerator.Next(1, 22);
+                        int damageClass3 = damageGenerator.Next(5, 16);
+                        int damageClass4 = damageGenerator.Next(0, 999999);
+
+                        // jämför inputLength med det bestämda intervallet.
+                        if (botClassChosen == "1") {
+                            return damageClass1;
+                        }
+
+                        if (botClassChosen == "2") {
+                            return damageClass2;
+                        }
+
+                        if (botClassChosen == "3") {
+                            return damageClass3;
+                        }
+
+                        else {
+                            return damageClass4;
+                        }
+                    }
+
+                    int damage = GetPlayerDamage(classChosen);
+                    int botDamage = GetBotDamage(botClassChosen);
+
+                    botHealth = botHealth - damage;
+                    playerHealth = playerHealth - botDamage;
+
+                    if (playerHealth < 1) {
+                        Console.WriteLine("Your enemy strikes you down and wins!");
+                        inMenu = true;
+                        gameStarted = false;
+                    }
+
+                    else if (botHealth < 1) {
+                        Console.WriteLine("You strike down your enemy and win!");
+                        inMenu = true;
+                        gameStarted = false;
+                    }
+
+                    if (gameStarted) {
+                        Console.WriteLine("You strike and deal " + damage + " damage. Your enemy now has " + botHealth + " health.");
+                        Console.WriteLine("");
+                        Console.WriteLine("Press any key to continue.");
+                        Console.ReadLine();
+
+                        Console.WriteLine("Your enemy strikes and deals " + botDamage + " damage. You now have " + playerHealth + " health.");
+                        Console.WriteLine("");
+                        Console.WriteLine("Press any key to continue.");
                         Console.ReadLine();
                         Console.Clear();
                     }
-                }
 
+                    if (gameStarted) {
+                        Console.WriteLine("==========================");
+                        Console.WriteLine("");
+                        Console.WriteLine("Your health: " + playerHealth);
+                        Console.WriteLine("Enemy health: " + botHealth);
+                        Console.WriteLine("");
+                        Console.WriteLine("==========================");
+                        Console.WriteLine("");
+                    }
+                }
 
                 Console.ReadLine();
                 Console.Clear();
@@ -316,12 +407,3 @@ namespace Slutprojekt
             }
         }
     }
-
-
-
-
-
-
-
-
-// skriv metod för health och för attacker
